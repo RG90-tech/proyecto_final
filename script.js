a5154111a7d55ccafe304f92fdb1676c35241873
Randy Garcia <imagine_405@hotmail.com>
2026-09-01 20:41:56 -0600

    se agregan cambios al main y script

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const telefonoWhatsApp = "525518054338";
 
 const mensajeWhatsApp =
-    "Hola, quiero agendar mi evento con Sonido Breakbeats. Me gustaría recibir más información sobre los paquetes y disponibilidad.";
+  "Hola, quiero agendar mi evento con Sonido Breakbeats. Me gustaría recibir más información sobre los paquetes y disponibilidad.";
 
 const botonAgendar = document.querySelector(".boton-agendar");
 
 botonAgendar.addEventListener("click", function () {
+  const mensajeCodificado = encodeURIComponent(mensajeWhatsApp);
 
-    const mensajeCodificado = encodeURIComponent(mensajeWhatsApp);
-    
-    const urlWhatsApp =
-        `https://wa.me/${telefonoWhatsApp}?text=${mensajeCodificado}`;
-    window.open(urlWhatsApp, "_blank");
-    
+  const urlWhatsApp = `https://wa.me/${telefonoWhatsApp}?text=${mensajeCodificado}`;
+  window.open(urlWhatsApp, "_blank");
 });
